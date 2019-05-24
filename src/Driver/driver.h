e6da4304ac53a4325bde0434e170036fd5d4e43f
Rats <juliocesarmmc@hotmail.com>
2019-05-24 12:40:18 -0300

    desativando....

--- src/Driver/driver.h
+++ src/Driver/driver.h
@@ -8,13 +8,11 @@
 #define LEN_STR           128
 #define DR_ERRO_MEMORY    1
 
-// #define GET_WCRT()              &wcrt
-// #define GET_BUSLOAD()           &busload_simulated
-// #define GET_MAX_TIME_QUEUE()    &time_max_queue
+#define GET_WCRT()              &wcrt
+#define GET_BUSLOAD()           &busload_simulated
+#define GET_MAX_TIME_QUEUE()    &time_max_queue
 
-#define SYNC_FLAG_BEST()        logframes    = &log_flag;
-#define SYNC_ITER_BEST()        iterador_log = &log_frame;
-
+// #define SYNC_FLAG_BEST()        logframes    = &log_flag;
 
 void   start_can_simulated(char* path_arq, double time_simulation);
 
