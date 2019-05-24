008e4a02db3cff0e38fc84d4e268003ebf152cad
Rats <juliocesarmmc@hotmail.com>
2019-05-24 12:37:15 -0300

    Script de plot de imagem e gifs

---
+++ Otimizador/scripts/plot-all.sh
@@ -0,0 +1,13 @@
+for file in $(ls *.dat)
+do 
+	echo $file
+	Rscript plot-msg-log.r $file && convert -append ${file}-mensagens-*.png $file.png
+	rm -f ${file}-mensagens-*.png
+done
+ 
+QTDE=$(ls *.dat.png | wc -l)
+BASENAME=$(ls -1 *.dat.png | head -n1 | sed 's/-[0-9]*.dat.png//g')
+IMAGES=$(seq 1 $QTDE | xargs -I {} echo ${BASENAME}-{}.dat.png | tr '\n' ' ')
+nice convert -delay 50 $IMAGES ${BASENAME}.gif
+
+
