aa51ca521488e28cbf3452de6dc940be596f65e9
Rats <juliocesarmmc@hotmail.com>
2019-05-24 12:40:06 -0300

    novo run-sh

--- Otimizador/run-exp.sh
+++ Otimizador/run-exp.sh
@@ -16,14 +16,12 @@ ARQ_CONFIG="config.txt"
 ARQ_TMP_ODBC="Tempos.dat"
 ARQ_RESULTADOS="Result.dat"
 ARQ_LOGOBJ='LogOBJ.dat'
-ARQ_BEST='LogBEST.dat'
+ARQ_BEST='LogBEST'
 
 Path_Bin='../Bin'
-Path_Result='Results/'
-Path_LOGOBJ='LogOBJ/'
-Path_BEST='LogBest/'
-
-Rdata=$(date +%s)
+Path_Result='Results'
+Path_LOGOBJ='LogOBJ'
+Path_BEST='LogBest'
 
 # Executa a aplicacao a quantidade de repeticoes especificada
 CONTADOR=0
@@ -32,6 +30,6 @@ do
 	data=$(date +%s)
 	echo "Execucao No. $CONTADOR"
 	echo
-	$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result$Rdata-$ARQ_RESULTADOS $Path_LOGOBJ$data-$ARQ_LOGOBJ $Path_BEST$data-$ARQ_BEST
+	$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS $Path_LOGOBJ/$data-$ARQ_LOGOBJ $Path_BEST/$data-$ARQ_BEST
 	let CONTADOR=CONTADOR+1
 done
