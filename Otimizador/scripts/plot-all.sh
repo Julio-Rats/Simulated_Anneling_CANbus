a8546ff2cd0099183453e5a2fa6e12f7c75a06d0
Rats <juliocesarmmc@hotmail.com>
2019-05-25 11:41:23 -0300

    script para criar gif, e config com entrada para escolher geração da primeira solução

--- Otimizador/scripts/plot-all.sh
+++ Otimizador/scripts/plot-all.sh
@@ -1,13 +1,11 @@
 for file in $(ls *.dat)
-do 
+do
 	echo $file
 	Rscript plot-msg-log.r $file && convert -append ${file}-mensagens-*.png $file.png
 	rm -f ${file}-mensagens-*.png
 done
- 
+
 QTDE=$(ls *.dat.png | wc -l)
 BASENAME=$(ls -1 *.dat.png | head -n1 | sed 's/-[0-9]*.dat.png//g')
 IMAGES=$(seq 1 $QTDE | xargs -I {} echo ${BASENAME}-{}.dat.png | tr '\n' ' ')
-nice convert -delay 50 $IMAGES ${BASENAME}.gif
-
-
+nice convert -delay 50 -loop 0 $IMAGES ${BASENAME}.gif
