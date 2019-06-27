548fb019fac303005f9d79ad8d4e8a520f274791
Rats <juliocesarmmc@hotmail.com>
2019-06-27 17:40:37 -0300

    entrada e script para validaçao e testes

--- Otimizador/run-exp.sh
+++ Otimizador/run-exp.sh
@@ -30,9 +30,9 @@ do
 	data=$(date +%s)
 	echo "Execucao No. $CONTADOR"
 	echo
-	#$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS $Path_LOGOBJ/$data-$ARQ_LOGOBJ $Path_BEST/$data-$ARQ_BEST
+	$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS $Path_LOGOBJ/$data-$ARQ_LOGOBJ $Path_BEST/$data-$ARQ_BEST
 	# $Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS $Path_LOGOBJ/$data-$ARQ_LOGOBJ
 	#$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS $Path_BEST/$data-$ARQ_BEST
-	$Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS
+	# $Path_Bin/otimizador $ARQ_CONFIG $ARQ_TMP_ODBC $Path_Result/$data-$ARQ_RESULTADOS
 	let CONTADOR=CONTADOR+1
 done
