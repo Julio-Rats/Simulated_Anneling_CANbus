fe9d8d90750fee11e49d9f27c1040f1f4b49a930
Rats <juliocesarmmc@hotmail.com>
2019-06-28 18:06:27 -0300

    estruturada no codigo

--- src/Simulated Anneling/simulated.h
+++ src/Simulated Anneling/simulated.h
@@ -53,21 +53,23 @@
 /*  e parametros associados                                                              */
 /*                                                                                       */
 /*****************************************************************************************/
-//(Julio)
+
 #define TIME_CAN_SIMULATED         120000  /* Tempo que o simulador ira emular*/
 #define PORC_START_DELAY           1       /* Maior porpoção escolhida para StartDelay*/
 #define ESCALAR_WCRT               100000  /* Escalar F()Objetiva para WCRT*/
 #define ESCALAR_QUEUE              10      /* Escalar F()Objetiva para tempo max da fila*/
 #define ESCALAR_DELAY              10      /* Escalar F()Objetiva para StartDelay*/
-#define LIMIT_DELAY                7
-//
+#define LIMIT_DELAY                7       /* Tempo minimo para um StartDelay */
 
 /* Habilita ou desabilita os modos verbose */
 #define SA_VERBOSE                 FALSE
 #define SA_VERBOSE_PROB						 FALSE
 
+/* Habilita escrita em arquivos de Logs */
 #define SA_GRAVA_BEST							 FALSE
 #define SA_GRAVA_OBJ               TRUE
+
+/* Habilita limite minimo de StartDelay*/
 #define SET_LIMIT_DELAY            FALSE
 
 /* Habilita ou desabilita o modo de gravação do vizinho */
@@ -75,7 +77,6 @@
 #define SA_TIMER_SIMULATED         0
 
 
-//(Julio)
 /*****************************************************************************************/
 /*                                                                                       */
 /*  Defines para busca e perturbação dos vizinhos                                         */
@@ -89,7 +90,7 @@
 #define SA_CNF_PERT_RANDOM             0x02
 #define SA_CNF_START_RANDOM            0x01
 #define SA_CNF_START_ZERO              0x02
-//
+
 
 /*****************************************************************************************/
 /*                                                                                       */
@@ -127,12 +128,6 @@ typedef struct
 	char*     pNome;     /* String com nome da mensagem */
 }StSaMsgPar;
 
-/* Array de parametros de mensagens */
-StSaMsgPar* pSaMsgParArray = NULL;
-
-/* Numero de mensagens consideradas pelo arquivo de configuracao */
-u_int16_t   SaNumMsgCan    = 0L;
-
 /* Slot com proposta de temporizacao para uma mensagem CAN */
 typedef struct
 {
@@ -150,6 +145,12 @@ typedef struct
 	double          Busload;     /* busload dessa solução*/
 	StSaMsgTmrSlot* pSol;        /* Vetor de slots com parametros propostos para solucao */
 }StSaSolucao;
+
+/* Array de parametros de mensagens */
+StSaMsgPar* pSaMsgParArray = NULL;
+
+/* Numero de mensagens consideradas pelo arquivo de configuracao */
+u_int16_t   SaNumMsgCan    = 0L;
 
 /* Solucoes usadas durante o processamento do Simulated Annealing */
 StSaSolucao* pSaMelhor             = NULL;
@@ -172,11 +173,10 @@ u_int8_t     SaMetodoPert          = SA_CNF_PERT_INCRIMENT;
 u_int8_t     SaMetodoInicial       = SA_CNF_START_ZERO;
 
 /*Descritores de arquvos, para os logs, modos verbose*/
-FILE*    	   Arq_OBJ            = NULL;
+FILE*    	   Arq_OBJ                = NULL;
 FILE*    	   Arq_Best               = NULL;
 
 static u_int32_t log_frame          = 1;
-//
 
 /* Controla posicoes sorteadas pelo SA */
 u_int8_t* pSaBitPosicao;
